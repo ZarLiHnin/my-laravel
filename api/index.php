5c44082494e0f59d2fa39084d435325688450558
ZarLiHnin <zarlehninn@gmail.com>
2025-10-02 01:58:07 +0900

    api fix

--- api/index.php
+++ api/index.php
@@ -1,13 +1,2 @@
 <?php
-// api/index.php
-
-// Request の URI を書き換える
-$request = Illuminate\Http\Request::capture();
-$path = $request->getPathInfo();
-
-// /api プレフィックスを削除
-if (str_starts_with($path, '/api')) {
-    $request->server->set('REQUEST_URI', substr($path, 4) ?: '/');
-}
-
 require __DIR__ . '/../public/index.php';
