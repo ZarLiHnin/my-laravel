644f956f0b01793125badf61c3f226abed4b9a8e
ZarLiHnin <zarlehninn@gmail.com>
2025-10-02 02:17:43 +0900

    api fix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-    return response()->json(['message' => 'Hello from Laravel!']);
+    return response()->json(['message' => 'Hello from Laravel!'])
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 });
